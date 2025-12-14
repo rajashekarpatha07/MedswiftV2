8eb89e3d1a3c76fc28237cdd61a99d39f9147e6d
rajashekarpatha07 <rajashekarpatha07@gmail.com>
2025-12-14 11:47:53 +0530

    feat: implement server setup with Express, MongoDB connection, and error handling; add JWT authentication utilities

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log("Hello world")
+import { app } from "./app.js";
+import dotenv from "dotenv";
+import { ConnectDb } from "./config/Dbconnecton.js";
+
+dotenv.config({
+  path: "./.env",
+});
+const PORT = process.env.PORT;
+const StartServer = async () => {
+  try {
+    await ConnectDb();
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.log("Error in connection to Database: ", error);
+    process.exit(1)
+  }
+};
+
+StartServer()
